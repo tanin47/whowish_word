a9520dc6f605155f2b782cb276cbb67f7edf638d
Tanin <tanin@tanin-VGN-TT2.(none)>
2012-07-15 00:57:20 +0700

    Fix Interpolation

--- lib/whowish_word/whowish_word.rb
+++ lib/whowish_word/whowish_word.rb
@@ -31,10 +31,28 @@ module WhowishWord
 
     I18n::MissingTranslation::Base.class_eval do
       def html_message
-        key = keys.last.to_s.gsub('_', ' ').gsub(/\b('?[a-z])/) { $1.capitalize }
+        key = keys.last.to_s.gsub('_', ' ').gsub(/\b('?[a-z])/) { $1 }
         key
       end
     end
+
+    I18n.class_eval do
+      class << self
+        def interpolate_hash(string, values)
+          string.gsub(I18n::INTERPOLATION_PATTERN) do |match|
+            if match == '%%'
+              '%'
+            else
+              key = ($1 || $2).to_sym
+              value = values.key?(key) ? values[key] : "%{#{key}}" #raise(MissingInterpolationArgument.new(values, string))
+              value = value.call(values) if value.respond_to?(:call)
+              $3 ? sprintf("%#{$3}", value) : value
+            end
+          end
+        end
+      end
+    end
+
   end
 
   extend self
