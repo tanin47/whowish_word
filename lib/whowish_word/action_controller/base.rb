39621735c6d925de5043c27a74b82dab67e998e3
Michael Wheeler <mwheeler@g2crowd.com>
2013-05-16 20:58:39 -0500

    Adding an action view function; makes it safer when config has not been set
    Conflicts:
    	lib/whowish_word/action_view/base.rb

--- lib/whowish_word/action_controller/base.rb
+++ lib/whowish_word/action_controller/base.rb
@@ -1,7 +1,7 @@
 if defined?(ActionController) and defined?(ActionController::Base)
   class ActionController::Base
     include WhowishWord::Constant
-    
+
     prepend_before_filter :initialize_whowish_word
     helper_method :whowish_word
     attr_accessor :whowish_word_config
@@ -13,49 +13,5 @@ if defined?(ActionController) and defined?(ActionController::Base)
     def whowish_word
       return @whowish_word_config
     end
-
-    # alias_method :previous_t, :t
-    # def t(uid, *variables)
-    #   if @whowish_word_config.edit_mode == true
-    #     s = PREFIX + \
-    #          SEPARATOR + \
-    #          scope_key_by_partial(uid.to_s) + \
-    #          SEPARATOR + \
-    #          previous_t(uid)
-    #     return "<dfn>#{s}</dfn>".html_safe
-    #   else
-    #     return previous_t(uid, *variables)
-    #   end
-    # end
-
-    # def ta(uid, *variables)
-    #   if @whowish_word_config.edit_mode == true
-    #     s = PREFIX + \
-    #          SEPARATOR + \
-    #          scope_key_by_partial(uid.to_s) + \
-    #          SEPARATOR + \
-    #          previous_t(uid)
-    #     return s.html_safe
-    #   else
-    #     return previous_t(uid, *variables)
-    #   end
-    # end
-
-    # def word_for(namespace, id, *variables)
-    #   if @whowish_word_config.edit_mode == true
-    #     return WhowishWord.word_for_in_edit_mode(namespace, id, @whowish_word_config.locale, *variables)
-    #   else
-    #     return WhowishWord.word_for(namespace, id, @whowish_word_config.locale, *variables)
-    #   end
-    # end
-    
-    
-    # def word_for_attr(namespace, id, *variables)
-    #   if @whowish_word_config.edit_mode == true
-    #     return WhowishWord.word_for_attr_in_edit_mode(namespace, id, @whowish_word_config.locale, *variables)
-    #   else
-    #     return WhowishWord.word_for_attr(namespace, id, @whowish_word_config.locale, *variables)
-    #   end
-    # end
   end
 end
